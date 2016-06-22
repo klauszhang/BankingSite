68f04e64f48eb1a9c5e5cdd9eb11454ed04e59b8
Hao Zhang <klaushaozhang@gmail.com>
2016-06-23 11:59:38 +1200

    test trigger

--- BankingSite.FunctionalUITests/PageObjectModels/AcceptedPage.cs
+++ BankingSite.FunctionalUITests/PageObjectModels/AcceptedPage.cs
@@ -3,7 +3,7 @@ using TestStack.Seleno.PageObjects;
 
 namespace BankingSite.FunctionalUITests.PageObjectModels
 {
-  public class AcceptedPage : Page
+  public class AcceptedPage : Page 
   {
     public string AcceptedMessage => Find.Element(By.Id("acceptMessage")).Text;
   }
